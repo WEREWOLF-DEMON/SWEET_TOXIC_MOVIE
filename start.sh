06cda37253983c49acdc107f52533a117bf95926
SWEET-TOXIC-DEVIL <153902241+SWEET-TOXIC-DEVIL@users.noreply.github.com>
2024-03-11 09:02:12 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/SWEET-TOXIC-DEVIL/SWEET_TOXIC_MOVIE.git /SWEET_TOXIC_MOVIE 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /SWEET_TOXIC_MOVIE 
 fi
-cd /VJ-FILTER-BOT 
+cd /SWEET_TOXIC_MOVIE 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
