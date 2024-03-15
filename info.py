5e2818511112d7a3447f4438bab0221b70272a93
SWEET-TOXIC-DEVIL <153902241+SWEET-TOXIC-DEVIL@users.noreply.github.com>
2024-03-15 10:48:23 +0530

    𝙎𝙒𝙀𝙀𝙏_𝙏𝙊𝙓𝙄𝘾_𝙈𝙊𝙑𝙄𝙀🫀🩸🖤

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sweet_toxic_devil:sweet_toxic_devil@sweettoxicdevil.wnaae1y.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sweet_toxic_Movie:sweet_toxic_Movie@sweettoxicdevil.citaw4q.mongodb.net/?retryWrites=true&w=majority&appName=SweetToxicDevil")
 DATABASE_NAME = environ.get('DATABASE_NAME', "sweettoxicdevil")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
